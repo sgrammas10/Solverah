228d23c407e467b25c6512e781ce144297929112
gramms <gramms@rpi.edu>
2025-12-29 17:44:54 -0500

    added frontend url from vers

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ CORS(
         "origins": [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
+            "https://solverah.vercel.app"
         ]
     }},
     supports_credentials=True,
