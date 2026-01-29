da8778066089c3ed5729ae2f9a81a0699fd8bfcb
gramms <gramms@rpi.edu>
2026-01-29 11:04:58 -0500

    added project root bc env.py was unable to find model bc it isnt in project root

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,15 @@
 from __future__ import with_statement
 
 import os
+import sys
+from pathlib import Path
 
 from alembic import context
 from sqlalchemy import engine_from_config, pool
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
 
 from model import db
 
