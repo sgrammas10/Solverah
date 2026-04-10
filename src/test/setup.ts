fca2db6ffa513726839f4f8a243379cd559caa3d
Will Gaca <will.gaca@gmail.com>
2026-04-10 16:52:26 -0400

    errors from unit test

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,16 @@
 ﻿import "@testing-library/jest-dom";
+
+// JSDOM does not implement window.matchMedia
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => false,
+  }),
+});
